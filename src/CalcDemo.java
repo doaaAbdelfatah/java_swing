6b24db4ae9dbcf1cedf1ee13db5da70ada703019
Doaa Abd Elfatah <doaa.abdelftah@gmail.com>
2024-06-11 12:02:38 +0300

    session 144  cont. Calc

--- src/CalcDemo.java
+++ src/CalcDemo.java
@@ -6,7 +6,15 @@ public class CalcDemo extends JFrame {
     private  JButton [] btnNumbers ;
     private  JButton btnDot ;
     private  JButton btnEqual ;
+    private  JButton btnPercent ;
+    private  JButton btnClear ;
+    private  JButton btnClearAll ;
+    private  JButton btnBackSpace ;
     private  JButton []  btnOperation;
+    private String selectedOperation ; // + , - , /,*
+    private double firstNumber;
+    private boolean hasOperation;
+
     public CalcDemo(){
         setTitle("Calculator");
         setBounds(400,15,350,400);
@@ -21,14 +29,59 @@ public class CalcDemo extends JFrame {
         tf.setPreferredSize(new Dimension(300 , 50) );
         tf.setEditable(false);
         panelN.add(tf);
-
         ////////////////////////////////////////////////////
+
+        JPanel panelCenter = new JPanel(new BorderLayout());
+        this.getContentPane().add(panelCenter);
+
+        JPanel panelNourth = new JPanel(new GridLayout(1,4 ,3,3));
+        panelNourth.setPreferredSize(new Dimension(350,60));
+
+        btnPercent = new JButton("%");
+        btnPercent.setFont(new Font("Tahoma" , Font.BOLD , 24));
+        btnPercent.setBorder(BorderFactory.createRaisedBevelBorder());
+        btnPercent.setBackground(new Color(230,230,230));
+        panelNourth.add(btnPercent);
+
+        btnClear = new JButton("C");
+        btnClear.setFont(new Font("Tahoma" , Font.BOLD , 24));
+        btnClear.setBorder(BorderFactory.createRaisedBevelBorder());
+        btnClear.setBackground(new Color(230,230,230));
+        panelNourth.add(btnClear);
+
+        btnClearAll = new JButton("CE");
+        btnClearAll.setFont(new Font("Tahoma" , Font.BOLD , 24));
+        btnClearAll.setBorder(BorderFactory.createRaisedBevelBorder());
+        btnClearAll.setBackground(new Color(230,230,230));
+        panelNourth.add(btnClearAll);
+
+
+        btnBackSpace = new JButton("<-");
+        btnBackSpace.addActionListener(e->{
+            String txt = tf.getText();
+            tf.setText(txt.substring(0,txt.length()-1));
+            if (tf.getText().equals("")) tf.setText("0");
+        });
+        btnBackSpace.setFont(new Font("Tahoma" , Font.BOLD , 24));
+        btnBackSpace.setBorder(BorderFactory.createRaisedBevelBorder());
+        btnBackSpace.setBackground(new Color(230,230,230));
+        panelNourth.add(btnBackSpace);
+
+        panelCenter.add(panelNourth ,BorderLayout.NORTH);
         JPanel panelC = new JPanel(new GridLayout(4,3 , 3,3));
-        this.getContentPane().add(panelC);
+        panelCenter.add(panelC);
 
         btnNumbers = new JButton[10];
         for (int i = 9; i >=0 ; i--) {
             btnNumbers [i] = new JButton(i + "");
+            btnNumbers [i].addActionListener(e -> {
+                if (tf.getText().equals("0")  || hasOperation ){
+                    tf.setText(e.getActionCommand());
+                    hasOperation=false;
+                }else{
+                    tf.setText(tf.getText() + e.getActionCommand());
+                }
+            });
             btnNumbers [i].setFont(new Font("Tahoma" , Font.BOLD , 24));
             btnNumbers [i].setBorder(BorderFactory.createRaisedBevelBorder());
             btnNumbers [i].setBackground(new Color(230,230,230));
@@ -42,6 +95,23 @@ public class CalcDemo extends JFrame {
         panelC.add(btnDot);
 
         btnEqual = new JButton("=");
+        btnEqual.addActionListener(e->{
+            double secoundNumber = Double.parseDouble( tf.getText());
+            switch (selectedOperation){
+                case  "+":
+                    tf.setText((firstNumber + secoundNumber) +"");
+                    break;
+                case  "-":
+                    tf.setText((firstNumber - secoundNumber) +"");
+                    break;
+                case  "*":
+                    tf.setText((firstNumber * secoundNumber) +"");
+                    break;
+                case  "/":
+                    tf.setText((firstNumber / secoundNumber) +"");
+                    break;
+            }
+        });
         btnEqual.setFont(new Font("Tahoma" , Font.BOLD , 24));
         btnEqual.setBorder(BorderFactory.createRaisedBevelBorder());
         btnEqual.setBackground(new Color(230,230,230));
@@ -50,15 +120,22 @@ public class CalcDemo extends JFrame {
 
         JPanel panelE = new JPanel(new GridLayout(4,1,3,3));
         panelE.setPreferredSize(new Dimension(80,300));
-        this.getContentPane().add(panelE, BorderLayout.EAST);
+        panelCenter.add(panelE, BorderLayout.EAST);
         btnOperation = new JButton[4];
         String [] op = {"/" , "*" , "-" ,"+"};
         for (int i = 0; i < op.length ; i++) {
             btnOperation [i] = new JButton(op[i]);
+            btnOperation[i].addActionListener(e->{
+               selectedOperation = e.getActionCommand(); // + - * /
+               firstNumber = Double.parseDouble( tf.getText());
+               hasOperation = true;
+            });
             btnOperation [i].setFont(new Font("Tahoma" , Font.BOLD , 24));
             btnOperation [i].setBorder(BorderFactory.createRaisedBevelBorder());
             btnOperation [i].setBackground(new Color(230,230,230));
             panelE.add(btnOperation [i]);
+
+
         }
     }
 
---
+++ src/StringDemo.java
@@ -0,0 +1,7 @@
+public class StringDemo {
+    public static void main(String[] args) {
+        String name = "Mohamed";
+        System.out.println(name.substring(0,name.length()-2));
+
+    }
+}
